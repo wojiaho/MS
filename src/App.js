fe1504e7827591f0128d5f7afad7df9ea2dba5cb
XuHui0421 <451476398@qq.com>
2019-03-28 15:53:52 +0800

    修改框架配置

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
+import NavLeft from './components/navLeft';
 
 class App extends Component {
   render() {
     return (
-      <div>hello world!</div>
+      <div className="page-wrapper">
+        <div  className="nav-left">
+          <NavLeft/>
+        </div>
+        <div className="main">
+          <Header/>
+          <div>container</div>
+          <Footer/>
+        </div>
+      </div>
     );
   }
 }
